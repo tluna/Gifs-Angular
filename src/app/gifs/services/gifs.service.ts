4db6918cdd8db33fcaee7d7fca63d62ef19f109d
Theddy Luna <theddyluna@gmail.com>
2021-02-02 18:52:45 -0500

    primer commit

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,57 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private apiKey:string = 'RaUfd4Z9v90dGixos8ZtKVJpcoJa3P3W';
+  private servicioUrl:string = 'https://api.giphy.com/v1/gifs';
+  private _historial:string[]=[]
+
+  // TODO: Cambiar any por su tipo correspondiente
+  public resultados:Gif[]=[];
+
+  get historial(){
+    return [...this._historial];
+  }
+
+  constructor(private http:HttpClient){
+    
+    this._historial = JSON.parse(localStorage.getItem('historial')!)||[];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!)||[];
+    // if(localStorage.getItem('historial')){
+    //   this._historial = JSON.parse(localStorage.getItem('historial')!);
+    // }
+
+  }
+
+  buscarGifs(query:string){
+
+    query = query.trim().toLowerCase();
+
+    if( !this._historial.includes(query) ){
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0,10);
+
+      localStorage.setItem('historial',JSON.stringify(this._historial) );
+    }
+
+    const params = new HttpParams()
+      .set('api_key',this.apiKey)
+      .set('limit','10')
+      .set('q',query);
+
+    console.log(params.toString);
+
+    this.http.get<SearchGifsResponse>(`${this.servicioUrl}/search`,{params})
+      .subscribe((resp) =>{
+        this.resultados=resp.data;
+        localStorage.setItem('resultados',JSON.stringify(this.resultados));
+      })
+   
+  }
+
+}
